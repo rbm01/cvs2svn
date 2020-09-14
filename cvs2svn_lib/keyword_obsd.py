2243b1b5f0bc3d90ecc53197afa36b440aca2706
Robert Mills <rmills@gigabyte.homenet1.au>
2020-09-14 19:05:58 +1000

    Use lookahead assertion to ensure a non-alphanumeric character follows trailing '$'.

--- cvs2svn_lib/keyword_obsd.py
+++ cvs2svn_lib/keyword_obsd.py
@@ -34,8 +34,8 @@ date_fmt = date_fmt_old
 
 _kws = 'Author|Date|Header|Id|Locker|Log|Mdocdate|Name|OpenBSD|RCSfile|Revision|Source|State'
 _kw_re  = re.compile(r'\$(' + _kws + r')\b:?[^$\n]*?(?<!\\)\$')
-_kwo_re = re.compile(r'\$(' + _kws + r')\b(:?[^$\n]*)?' \
-                     + r"(?<![.'" + r'"\\])\$[^\w]'
+_kwo_re = re.compile(r'\$(' + _kws + r')\b([^$\n]*)?' \
+                     + r"(?<![.'" + r'"\\])\$(?=\W)'
                      )
 # Example            r'\$(Author)\b(:?[^$\n]*)?(?<!\\)\$'
 
