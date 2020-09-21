cbcc4ee99d2001b4260e82764b1402e2fb5887da
Robert Mills <rmills@gigabyte.homenet1.au>
2020-09-21 16:12:35 +1000

    Now only check for keywords in the first 50 lines of a file.

--- cvs2svn_lib/rcs_stream.py
+++ cvs2svn_lib/rcs_stream.py
@@ -27,6 +27,9 @@ from cvs2svn_lib.keyword_obsd import expand_keywords
 # Print debug messages: 1 - enable debug messages   0 - disable debug messages
 debug = 0
 
+# Set a limit on the number of lines that are checked for keywords
+maxKeywordLines = 50
+
 def msplit(s):
   """Split S into an array of lines.
 
@@ -317,7 +320,10 @@ class RCSStream:
 
     E.g., '$Author$' -> '$Author: jrandom $'."""
 
-    i = 0; num_lines = len(self._lines)
+    num_lines = len(self._lines)
+    if num_lines > maxKeywordLines:
+      num_lines = maxKeywordLines
+    i = 0;
     if debug: print "RCSStream.expand_keywords(): About to expand " \
        + str(num_lines) + " lines."
     while i < num_lines:
@@ -335,7 +341,10 @@ class RCSStream:
     """Collapse CVS keywords in the current file content.  This must
     be performed after diffs were applied to the file content."""
 
-    i = 0; num_lines = len(self._lines)
+    num_lines = len(self._lines)
+    if num_lines > maxKeywordLines:
+      num_lines = maxKeywordLines
+    i = 0;
     while i < num_lines:
       # Quick check for presence of keyword. Here we check if the
       # string "self._lines[i]" contains at least two '$' characters.
